f8687b0823d8df9d55673145664500118c0346a1
khalid7487 <khalid15-7487@diu.edu.bd>
2019-12-17 19:12:22 +0600

    update

--- Online_shopping/src/java/dao/AddDao.java
+++ Online_shopping/src/java/dao/AddDao.java
@@ -10,12 +10,18 @@ import util.HibernateUtil;
  * @author KHALID
  */
 public class AddDao {
-    public Category addCategory(Category cat){
-        SessionFactory factory=HibernateUtil.getSessionFactory();
-        Session session=factory.openSession();
-        session.save(cat);
-        session.beginTransaction();
-        session.getTransaction().commit();
-        return  cat;
+
+    public boolean addCategory(Category cat) {
+        try {
+            SessionFactory factory = HibernateUtil.getSessionFactory();
+            Session session = factory.openSession();
+            session.save(cat);
+            session.beginTransaction();
+            session.getTransaction().commit();
+            return true;
+        } catch (Exception e) {
+            return  false;
+        }
+
     }
 }
