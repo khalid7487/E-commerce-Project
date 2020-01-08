5a44fe73bfbe010257a326a28169419c6f09622d
khalid7487 <khalid15-7487@diu.edu.bd>
2020-01-08 17:30:40 +0600

    update display and view cart

---
+++ Online_shopping/src/java/cart/Item.java
@@ -0,0 +1,30 @@
+
+package cart;
+
+import entity.Product;
+
+/**
+ *
+ * @author KHALID
+ */
+public class Item {
+    private Product p;
+    private int quantity;
+
+    public Product getP() {
+        return p;
+    }
+
+    public void setP(Product p) {
+        this.p = p;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+    
+}
